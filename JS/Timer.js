58551b64c8c4e4a2c6b3d7fd786b69864fc20469
F e l p <lipjb@hotmail.com>
2023-05-07 23:41:46 -0300

    fix: atualizacoes no projeto ( novo modal confirm - pagina expira quando terminar o tempo )

--- JS/Timer.js
+++ JS/Timer.js
@@ -1,24 +1,35 @@
+
 function startTimer(duration, display) {
-    var timer = duration,
-      minutes,
-      seconds;
-    setInterval(function () {
-      minutes = parseInt(timer / 60, 10);
-      seconds = parseInt(timer % 60, 10);
-  
-      minutes = minutes < 10 ? "0" + minutes : minutes;
-      seconds = seconds < 10 ? "0" + seconds : seconds;
-  
-      display.textContent = minutes + ":" + seconds;
-  
-      if (--timer < 0) {
-        timer = duration;
-      }
-    }, 1000);
-  }
-  
-  window.onload = function () {
-    var Minutes = 60 * 2,
-      display = document.querySelector("#time");
-    startTimer(Minutes, display);
-  };
+  var timer = duration,
+    minutes,
+    seconds;
+  setInterval(function () {
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
+
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
+
+    display.textContent = minutes + ":" + seconds;
+
+    if (--timer < 0) {
+      timer = duration;
+    }
+  }, 1000); 
+
+  // Chamando a função de recarregar documento a cada x segundos 
+  setInterval(rechargePage, 120000);
+}
+
+window.onload = function () {
+  var Minutes = 60 * 2,
+    display = document.querySelector("#time");
+  startTimer(Minutes, display);
+};
+
+
+
+
+
+
+
--- JS/confirmModal.js
+++ JS/confirmModal.js
@@ -1,55 +1,82 @@
 const modalConfirm = function () {
-    swal({
-      title: "FINALIZAR PAGAMENTO?",
-      buttons: ["Não", "Sim"],
-    }).then((willDelete) => {
-      if (willDelete) {
-        swal("PAGAMENTO COM SUCESSO !", {
-          icon: "success",
-        });
-        setTimeout(() => {
-          document.formulario.submit();
-        }, 2000); // aguardando 2 segundos para envio do formulario.
-      } else {
-        swal("PAGAMENTO CANCELADO !", {
-          icon: "warning",
-        });
-      }
-    });
+  const swalWithBootstrapButtons = Swal.mixin({
+    customClass: {
+      confirmButton: 'btn btn-success',
+      cancelButton: 'btn btn-danger'
+    }    
+  })
+
+  swalWithBootstrapButtons.fire({
+    title: 'Confirmar pagamento?',
+    text: "Ao comfirmar o pagamento será efetuado!",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Sim, confirmar!',
+    cancelButtonText: 'Não, cancelar!',
+    reverseButtons: true
+  }).then((result) => {
+    if (result.isConfirmed) {      
+      swalWithBootstrapButtons.fire(
+        'Pronto!',
+        'Seu pagamento foi efetuado com sucesso.',
+        'success'
+      )
+      setTimeout(() => {
+        document.formulario.submit();
+      }, 3000); // aguardando 2 segundos para envio do formulario.
+    } else if (
+      /* Read more about handling dismissals below */
+      result.dismiss === Swal.DismissReason.cancel
+    ) {
+      swalWithBootstrapButtons.fire(
+        'Poxa :(',
+        'Seu pagemento foi cancelado.)',
+        'error'
+      )
+    }
+  })
+};
+
+
+const form = document.querySelector("form[name=formulario]");
+const input = document.querySelectorAll("input");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const firstInput = input[0];
+  if (firstInput.value.length < 16) {
+    return;
+  }
+
+  const secondInput = input[1];
+  const secondInputRegex = /^[A-Za-z\s]+$/; // permite letras e espaços.
+  if (!secondInputRegex.test(secondInput.value)) {
+    console.warn("Prencha somente letras");
+    return;
+  }
+
+  const thirdInput = input[2];
+  const thirdInputRegex = /^\d{3,4}$/; // permite até 4 dígitos numéricos.
+  if (!thirdInputRegex.test(thirdInput.value)) {
+    console.warn("Prencha todos os numeros");
+    return;
+
+  }
+
+  // zerando o timer após envio do form
+
+  window.onload = function () {
+    var Minutes = 5 * 2,
+      display = document.querySelector("#time");
+    startTimer(Minutes, display);
   };
-  
-  const form = document.querySelector("form[name=formulario]");
-  const input = document.querySelectorAll("input");
-  
-  form.addEventListener("submit", (event) => {
-    event.preventDefault();
-  
-    const firstInput = input[0];
-    if (firstInput.value.length < 16) {
-      return;
-    }
-  
-    const secondInput = input[1];
-    const secondInputRegex = /^[A-Za-z\s]+$/; // permite letras e espaços.
-    if (!secondInputRegex.test(secondInput.value)) {
-      return;
-    }
-  
-    const thirdInput = input[2];
-    const thirdInputRegex = /^\d{3,4}$/; // permite até 4 dígitos numéricos.
-    if (!thirdInputRegex.test(thirdInput.value)) {
-      return;
-    }   
-// zerando o timer após envio do form
 
-    window.onload = function () {
-      var Minutes = 60 * 2,
-        display = document.querySelector("#time");
-      startTimer(Minutes, display);
-    };
+  modalConfirm();
 
-    modalConfirm();
-    let changeColor = document.querySelector('.step .active');
-        changeColor.style.backgroundColor = "#6ec546";
-  });
-  
+  let changeColor = document.querySelector('.step .active');
+  changeColor.style.backgroundColor = "#6ec546";
+  changeColor.style.opacity = 0.8;
+});
